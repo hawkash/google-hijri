ce5aca30f8b0a2df4d8d0fb34748933f80716505
hawkash <37810955+hawkash@users.noreply.github.com>
2023-06-01 11:47:11 +0300

    Update hijri.js

--- hijri.js
+++ hijri.js
@@ -27,33 +27,29 @@
         'Dhuʻl-H.': 12
     };
 
-    // Function to convert Hijri to Gregorian - you'd need to replace this with a real implementation
     function hijriToGregorian(hijriYear, hijriMonth, hijriDay) {
-    var hijriStart = new Date(622, 7, 20); // The start of the Hijri calendar in Gregorian dates
-    var hijriElapsed = ((hijriYear - 1) * 354.37) + // Hijri years elapsed (354.37 days each)
-                       ((hijriMonth - 1) * 29.5) + // Hijri months elapsed (29.5 days each)
-                       (hijriDay - 1); // Hijri days elapsed
-    var gregorianDate = new Date(hijriStart.getTime() + hijriElapsed * 24 * 60 * 60 * 1000); // Convert elapsed days to milliseconds
-    return gregorianDate;
-}
+        var hijriStart = new Date(622, 7, 20);
+        var hijriElapsed = ((hijriYear - 1) * 354.37) +
+                           ((hijriMonth - 1) * 29.5) +
+                           (hijriDay - 1);
+        var gregorianDate = new Date(hijriStart.getTime() + hijriElapsed * 24 * 60 * 60 * 1000);
+        return gregorianDate;
+    }
 
-    // Target elements - Google Search Results
     var targetNodes = document.querySelectorAll('.VwiC3b .MUxGbd .wuQ4Ob .WZ8Tjf');
 
     targetNodes.forEach(function(targetNode) {
         var textContent = targetNode.textContent;
-        // If the text content has a Hijri date pattern (Short month name Day, Year AH)
         var regex = new RegExp(Object.keys(hijriMonths).join("|") + " \\d{1,2}, \\d{4} AH");
         if (regex.test(textContent)) {
             var hijriDates = textContent.match(regex);
             hijriDates.forEach(function(hijri) {
-                var parts = hijri.split(', ');
-                var monthDay = parts[0].split(' ');
-                var month = hijriMonths[monthDay[0]];
-                var day = monthDay[1];
-                var year = parts[1].replace(" AH", "");
-                var gregorian = hijriToGregorian(day, month, year);
-                textContent = textContent.replace(hijri, gregorian);
+                var parts = hijri.split(' ');
+                var day = parseInt(parts[1].replace(',', ''));
+                var month = hijriMonths[parts[0]];
+                var year = parseInt(parts[2]);
+                var gregorian = hijriToGregorian(year, month, day);
+                textContent = textContent.replace(hijri, gregorian.toLocaleDateString());
             });
             targetNode.textContent = textContent;
         }
